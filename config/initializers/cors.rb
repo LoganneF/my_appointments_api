587af08bf352f887e91a736f409cc1209b76cad5
LoganneF <loganne.featherston@gmail.com>
2020-05-13 16:37:32 -0600

    fixed cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://git.heroku.com/house-appointments-client.git'
+    origins 'https://git.heroku.com/house-appointments-client.git/'
 
     resource '*',
       headers: :any,
