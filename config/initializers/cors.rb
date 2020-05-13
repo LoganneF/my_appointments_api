44758783ace549cef244f6e49c715c32816565d5
LoganneF <loganne.featherston@gmail.com>
2020-05-13 15:44:46 -0600

    changed cors and proxy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001'
+    origins 'http://logannef.com/my_appointments_client.github.io/'
 
     resource '*',
       headers: :any,
